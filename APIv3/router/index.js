5ddf197af4ce7a555b1d9498ef4375e4f61dfb40
Brando S.A <141217206+sirDuuck@users.noreply.github.com>
2023-10-27 12:31:06 -0300

    12:30 27/10/2023

--- APIv3/router/index.js
+++ APIv3/router/index.js
@@ -4,16 +4,16 @@ import ControllerGeral from "../src/Controller/index.js";
 const renovacao = Router();
 
 //Post
-renovacao.post("/register/v2/:authorization/:nome/:cnpj/:email/:telefone", ControllerGeral.RenovacaoURL)
-renovacao.post("/register/:authorization", ControllerGeral.RenovacaoRegistroAuthorization)
-renovacao.post("/agenda", ControllerGeral.RenovacaoAgenda)
-renovacao.post("/registro", ControllerGeral.RenovacaoRegistro)
+renovacao.post("/register/v2/:authorization/:nome/:cnpj/:email/:telefone", ControllerGeral.RenovacaoURL) //ceerto
+renovacao.post("/register/:authorization", ControllerGeral.RenovacaoRegistroAuthorization) // Erro atravez da pasta "config"
+renovacao.post("/agenda", ControllerGeral.RenovacaoAgenda) //ceerto
+renovacao.post("/registro", ControllerGeral.RenovacaoRegistro) //esta dando erro no ERP_LOG que eu não locazei de onde ta viindo 
 
 
 
 //Get
-renovacao.get("/check/erp/:authorization", ControllerGeral.RenovacaoCheck)
-renovacao.get("/check/valid/:cnpj", ControllerGeral.RenovacaoCheckCnpj)
-renovacao.get("/list/user/max/polo", ControllerGeral.RenovacaoList)
+renovacao.get("/check/erp/:authorization", ControllerGeral.RenovacaoCheck) //ceerto
+renovacao.get("/check/valid/:cnpj", ControllerGeral.RenovacaoCheckCnpj) //ceerto
+renovacao.get("/list/user/max/polo", ControllerGeral.RenovacaoList) //ceerto
 
 export default renovacao
