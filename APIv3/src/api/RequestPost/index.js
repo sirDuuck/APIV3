851f284192057c5ed9d5aa1f3eccf6e36912660c
Brando S.A <141217206+sirDuuck@users.noreply.github.com>
2023-10-27 16:13:47 -0300

    oq eu coonseguia eu ja fiz 16:13

--- APIv3/src/api/RequestPost/index.js
+++ APIv3/src/api/RequestPost/index.js
@@ -2,22 +2,19 @@ import axios from "axios";
 import "dotenv/config";
 
 const RequestPost = async (url, data) => {
-  try {
-    const response = await axios({
-      method: "Post",
-      url: process.env.SERVE_CONSULT + url,
-      data,
-    });
-    if (response.data && response.data.id) {
-      console.log({ status: 200, message: 'Requisição bem-sucedida' });
-      return response.data.id;
-    } else {
-      throw new Error('ID not found in the response');
-    }
-  } catch (error) {
-    console.error({ status: 400, message: 'A requisição falhou' });
-    throw error;
-  }
-};
+    try {
+      const response = await axios({
+        method: "Post",
+        url: process.env.SERVE_CONSULT + url,
+        data,
+      });
 
-export default RequestPost
+      console.log({ status: 200, message: 'Requisição bem-sucedida' });
+      return response.data;
+    } catch (error) {
+      console.error({ status: 400, message: 'A requisição falhou' });
+      throw error; 
+    }
+  };
+
+  export default RequestPost
